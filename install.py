26b70c909b415a2aa59d7d355cd056020ea06b45
DeadSecurityTraceur <76254042+DedSec-Traceur@users.noreply.github.com>
2021-01-15 14:57:24 -0500

    Add files via upload

---
+++ install.py
@@ -0,0 +1,7 @@
+import os       
+
+os.system('git clone https://github.com/graylagx2/ApkBleach.git')
+os.system('git clone https://github.com/DedSec-Traceur/webassassin.git') 
+os.system('git clone https://github.com/DedSec-Traceur/DeadGoogleDocs.git') 
+os.system('git clone https://github.com/DedSec-Traceur/LiveHack.git') 
+
---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/graylagx2/ApkBleach.git .
+git clone https://github.com/DedSec-Traceur/webassassin.git .
+git clone https://github.com/DedSec-Traceur/DeadGoogleDocs.git .
+git clone https://github.com/DedSec-Traceur/LiveHack.git .
+
